0bb374134b242ac2904c7fb470bf7d678987bf72
Dan Henry <s3543854@student.rmit.edu.au>
2020-07-18 18:41:34 +1000

    convert team names

--- odds.py
+++ odds.py
@@ -2,6 +2,42 @@ import requests
 import json
 
 class OddsAPI:
+    def convert_team_name(self, str):
+        if str == "Essendon Bombers":
+            team = "Essendon"
+        elif str == "Western Bulldogs":
+            team = "Western Bulldogs"
+        elif str == "Brisbane Lions":
+            team = "Brisbane Lions"
+        elif str == "Greater Western Sydney Giants":
+            team = "GWS"
+        elif str == "Gold Coast Suns":
+            team = "Gold Coast"
+        elif str == "Sydney Swans":
+            team = "Sydney"
+        elif str == "North Melbourne Kangaroos":
+            team = "North Melbourne"
+        elif str == "Richmond Tigers":
+            team = "Richmond"
+        elif str == "Carlton Blues":
+            team = "Carlton"
+        elif str == "Port Adelaide Power":
+            team = "Port Adelaide"
+        elif str == "Hawthorn Hawks":
+            team = "Hawthorn"
+        elif str == "Melbourne Demons":
+            team = "Melbourne"
+        elif str == "Fremantle Dockers":
+            team = "Fremantle"
+        elif str == "West Coast Eagles":
+            team = "West Coast"
+        elif str == "Adelaide Crows":
+            team = "Adelaide"
+        elif str == "St Kilda Saints":
+            team = "St Kilda"
+
+        return team
+
     def get_odds(self, oddsType):
         matches = []
         print("DEBUG: oddsType = " + oddsType)
@@ -18,9 +54,8 @@ class OddsAPI:
                     hOdds = site['odds']['h2h'][0]
                     aOdds = site['odds']['h2h'][1]
 
-            match = {'home': {'team': m['teams'][0], 'odds': hOdds}, 'away': {'team': m['teams'][1], 'odds': aOdds}}
+            match = [{'team': self.convert_team_name(m['teams'][0]), 'odds': hOdds}, {'team': self.convert_team_name(m['teams'][1]), 'odds': aOdds}]
             matches.append(match)
-            print("DEBUG: match = " + str(match))
 
         dump = json.dumps(matches)
 
